5e63a8c212429eea399662810ca368ce1b758f9a
husainb0112 <husainb@nycstudents.net>
2024-09-17 02:04:52 +0000

    first commit

---
+++ MyClass.java
@@ -0,0 +1,17 @@
+public class MyClass
+{
+    public static void main(String[] args)
+    {
+        int num1 = 50;
+        int num2 = 51;
+        boolean fact = true;
+        double currentGrade = 50.5;
+        String current = "I have a ";
+        String desired = "I want a ";
+        int num3 = num1 + num2;
+        System.out.println("It is " + fact + " that " + desired + num3 + " in APCSA.");
+        boolean notFact = !fact;
+        System.out.print("It is " + notFact + " that " + current + currentGrade + " in APCSA.");
+
+    }
+}
